dc7fd653f6042d14820e21d4813bcd5f8ced1269
crotsos <cr409@cl.cam.ac.uk>
2012-02-27 21:13:11 +0000

    Adding licence to all source files

--- lib/controller.ml
+++ lib/controller.ml
@@ -1,5 +1,5 @@
 (* 
- * Copyright (c) 2005-2011 Charalampos Rotsos <cr409@cl.cam.ac.uk>
+ * Copyright (c) 2012 Charalampos Rotsos <cr409@cl.cam.ac.uk>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
--- lib/ofpacket.ml
+++ lib/ofpacket.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2011 Richard Mortier <mort@cantab.net>
+ * Copyright (c) 2012 Richard Mortier <mort@cantab.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
