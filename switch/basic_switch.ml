93bfd46f73d1271a318abdeba3a9c9c884a1fd10
crotsos <cr409@cl.cam.ac.uk>
2012-09-05 12:09:00 -0700

    Adding a simple example of an openflow switch

---
+++ switch/basic_switch.ml
@@ -0,0 +1,54 @@
+(* 
+ * Copyright (c) 2012 Charalampos Rotsos <cr409@cl.cam.ac.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Lwt
+open Unix
+open Printf
+open Net
+
+
+(****************************************************************
+ * OpenFlow Switch configuration 
+ *****************************************************************)
+
+let print_time () =
+  while_lwt true do
+    Lwt_unix.sleep 1.0 >>
+    return (printf "%03.6f: process running..\n%!" (Unix.gettimeofday ()))
+  done
+
+lwt () = 
+  let sw = Ofswitch.create_switch () in
+  try_lwt 
+    Manager.create 
+    (fun mgr interface id ->
+       match (Manager.get_intf_name mgr id) with 
+         | "0" ->
+             let ip = 
+               Nettypes.(
+                 (ipv4_addr_of_tuple (10l,0l,0l,1l),
+                  ipv4_addr_of_tuple (255l,255l,255l,0l), [])) in  
+               lwt _ = Manager.configure interface (`IPv4 ip) in
+               lwt _ = (Ofswitch.listen sw mgr (None, 6633) <&> 
+                       (print_time ())) in 
+                return ()
+      | _ -> 
+          return (Ofswitch.add_port mgr sw id)
+    )
+  with e ->
+    Printf.eprintf "Error: %s" (Printexc.to_string e); 
+    return ()
+
