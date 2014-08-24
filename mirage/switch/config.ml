633839e14c2d1665d01da6682ea0a9b1c0255717
Richard Mortier <mort@cantab.net>
2014-08-24 13:48:49 -0500

    switch: support now-packed `openflow` library

--- mirage/switch/config.ml
+++ mirage/switch/config.ml
@@ -3,8 +3,8 @@ open Mirage
 let main = foreign "Unikernel.Main" (console @-> stackv4 @-> network @-> network @-> job)
 
 let unix_libs =
-  match get_mode () with 
-  | `Unix -> ["mirage-clock-unix"] 
+  match get_mode () with
+  | `Unix -> ["mirage-clock-unix"]
   | _ -> []
 
 let net =
@@ -32,7 +32,7 @@ let stack console =
 
 let () =
   add_to_ocamlfind_libraries
-    ([ "tcpip.ethif"; "tcpip.tcpv4"; "tcpip.udpv4"; "tcpip.dhcpv4"; "tcpip.channel"; "cstruct.syntax" ] @ unix_libs);
+    ([ "tcpip.ethif"; "tcpip.tcpv4"; "tcpip.udpv4"; "tcpip.dhcpv4"; "tcpip.channel"; "cstruct.syntax"; "openflow" ] @ unix_libs);
 
   register "ofswitch" [
     main $ default_console $ (stack default_console) $ (netif "tap1") $ (netif "tap2")
--- mirage/switch/unikernel.ml
+++ mirage/switch/unikernel.ml
@@ -2,6 +2,7 @@ open V1_LWT
 open Lwt
 open Printf
 
+open Openflow
 open Ofswitch
 
 let red fmt    = Printf.sprintf ("\027[31m"^^fmt^^"\027[m")
@@ -20,11 +21,11 @@ module Main (C: CONSOLE)(S: STACKV4)(N1: NETWORK)(N2: NETWORK) = struct
 
 
   let or_error c name fn t =
-	fn t
-	>>= function
-	| `Error e -> fail (Failure ("error starting " ^ name))
-	| `Ok t -> C.log_s c (green "%s connected..." name) >>
-			   return t
+    fn t
+    >>= function
+    | `Error e -> fail (Failure ("error starting " ^ name))
+    | `Ok t -> C.log_s c (green "%s connected..." name) >>
+               return t
 
   module T = S.TCPV4
   module E = Ethif.Make(N1)
@@ -33,20 +34,20 @@ module Main (C: CONSOLE)(S: STACKV4)(N1: NETWORK)(N2: NETWORK) = struct
   let start console s n1 n2 =
 
   let netl = [n1; n2] in
-	(* mirage main module <= 1.2.0 doesn't accept list as the parameter.
-	   write input network devices as a list until mirage supprts list parameters *)
+    (* mirage main module <= 1.2.0 doesn't accept list as the parameter.
+       write input network devices as a list until mirage supprts list parameters *)
   let rec connect_ifs nl acc =
-	match nl with
-	| [] -> return acc
-	| n::t -> or_error console "ethernet interface" E.connect n >>= fun x -> (connect_ifs t (x::acc))
-		(* to do: ethernet interface -> name of the interface: tap1, tap2, ... *)
+    match nl with
+    | [] -> return acc
+    | n::t -> or_error console "ethernet interface" E.connect n >>= fun x -> (connect_ifs t (x::acc))
+        (* to do: ethernet interface -> name of the interface: tap1, tap2, ... *)
   in
-	C.log_s console (sprintf "IP address: %s\n" (Ipaddr.V4.to_string (S.IPV4.get_ipv4 (S.ipv4 s))))
-	>>
-	connect_ifs netl []
-	>>= fun e ->
-		Sw.create_switch (S.tcpv4 s) (* (switchaddr, netmask, gateway) *) (contaddr, contport) e
-	>>=
-	 fun fl -> 
-		return ();
+    C.log_s console (sprintf "IP address: %s\n" (Ipaddr.V4.to_string (S.IPV4.get_ipv4 (S.ipv4 s))))
+    >>
+    connect_ifs netl []
+    >>= fun e ->
+        Sw.create_switch (S.tcpv4 s) (* (switchaddr, netmask, gateway) *) (contaddr, contport) e
+    >>=
+     fun fl ->
+        return ();
 end
